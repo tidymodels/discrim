bc21c8021412ac79776ca252ce6afa9c5449be47
topepo <mxkuhn@gmail.com>
2019-10-08 09:56:48 -0400

    fda model

---
+++ R/discrim_flexible_data.R
@@ -0,0 +1,102 @@
+
+make_discrim_flexible <- function() {
+  parsnip::set_new_model("discrim_flexible")
+
+  parsnip::set_model_mode("discrim_flexible", "classification")
+
+  # ------------------------------------------------------------------------------
+
+  parsnip::set_model_engine("discrim_flexible", "classification", "earth")
+  parsnip::set_dependency("discrim_flexible", eng = "earth", pkg = "mda")
+  parsnip::set_dependency("discrim_flexible", eng = "earth", pkg = "earth")
+
+  parsnip::set_model_arg(
+    model = "discrim_flexible",
+    eng = "earth",
+    parsnip = "num_terms",
+    original = "nprune",
+    func = list(pkg = "dials", fun = "num_terms", range = c(2, 5)),
+    has_submodel = TRUE
+  )
+  parsnip::set_model_arg(
+    model = "discrim_flexible",
+    eng = "earth",
+    parsnip = "prod_degree",
+    original = "degree",
+    func = list(pkg = "dials", fun = "prod_degree"),
+    has_submodel = FALSE
+  )
+  parsnip::set_model_arg(
+    model = "discrim_flexible",
+    eng = "earth",
+    parsnip = "prune_method",
+    original = "pmethod",
+    func = list(pkg = "dials", fun = "prune_method"),
+    has_submodel = FALSE
+  )
+
+  parsnip::set_fit(
+    model = "discrim_flexible",
+    eng = "earth",
+    mode = "classification",
+    value = list(
+      interface = "formula",
+      protect = c("formula", "data"),
+      func = c(pkg = "mda", fun = "fda"),
+      defaults = list(method = quote(earth::earth))
+    )
+  )
+
+  parsnip::set_pred(
+    model = "discrim_flexible",
+    eng = "earth",
+    mode = "classification",
+    type = "class",
+    value = list(
+      pre = NULL,
+      post = NULL,
+      func = c(fun = "predict"),
+      args =
+        list(
+          object = quote(object$fit),
+          newdata = quote(new_data)
+        )
+    )
+  )
+
+  parsnip::set_pred(
+    model = "discrim_flexible",
+    eng = "earth",
+    mode = "classification",
+    type = "prob",
+    value = list(
+      pre = NULL,
+      post = prob_matrix_to_tibble,
+      func = c(fun = "predict"),
+      args =
+        list(
+          object = quote(object$fit),
+          newdata = quote(new_data),
+          type = "posterior"
+        )
+    )
+  )
+
+  parsnip::set_pred(
+    model = "discrim_flexible",
+    eng = "earth",
+    mode = "classification",
+    type = "raw",
+    value = list(
+      pre = NULL,
+      post = NULL,
+      func = c(fun = "predict"),
+      args =
+        list(
+          object = quote(object$fit),
+          newdata = quote(new_data)
+        )
+    )
+  )
+
+}
